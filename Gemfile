e022d9f04847bd90f1b427af5a1387f3522a697a
sleepingkingstudios <merlin@sleepingkingstudios.com>
2015-07-22 20:38:24 -0400

    Create Ruby library scaffold with RSpec support.

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# Gemfile
+
+source 'https://rubygems.org'
+
+gem 'rake',                        '~> 10.4', '>= 10.4.2'
+gem 'rspec',                       '~> 3.3'
+gem 'rspec-sleeping_king_studios', :path => '../rspec-sleeping_king_studios'
+
+gem 'byebug', '~> 5.0'
+
+ruby "2.2.2"
---
+++ spec/spec_helper.rb
@@ -0,0 +1,51 @@
+# spec/spec_helper.rb
+
+root_path = File.expand_path(File.join __dir__, '..')
+
+$: << File.join(root_path, 'lib')
+
+require 'rspec'
+
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+
+  # Limit a spec run to individual examples or groups you care about by tagging
+  # them with `:focus` metadata.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  # Allow more verbose output when running an individual spec file.
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
+  # Run specs in random order to surface order dependencies.
+  config.order = :random
+  Kernel.srand config.seed
+
+  # rspec-expectations config goes here.
+  config.expect_with :rspec do |expectations|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    expectations.syntax = :expect
+  end # expect_with
+
+  # rspec-mocks config goes here.
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    mocks.syntax = :expect
+
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended.
+    mocks.verify_partial_doubles = true
+  end # mock_with
+end # configure
+
+require 'rspec/sleeping_king_studios/all'
+require 'byebug'
+
+RSpec.configure do |config|
+  config.extend RSpec::SleepingKingStudios::Concerns::WrapExamples
+end # configure
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories.
+Dir[File.join(__dir__, *%w(support ** *.rb))].each { |f| require f }
